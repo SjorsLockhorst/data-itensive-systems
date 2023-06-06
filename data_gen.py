54d1d540a7518a209df46e8e103626241f11e867
Sjors Lockhorst <lockhorstsjors@gmail.com>
2023-06-06 23:14:05 +0200

    Finished all needed params for adding removing routes

--- data_gen.py
+++ data_gen.py
@@ -1,4 +1,5 @@
 import abc
+import time
 from dataclasses import dataclass
 from typing import (
     Any,
@@ -89,6 +90,10 @@ class Route(ImmutableModel):
     def __iter__(self):
         for trip in self.route:
             yield trip
+
+    def __len__(self):
+        return len(self.route)
+
 
 class NoisedRoute(Route):
     parent_route_id: int
@@ -222,24 +227,48 @@ def noise_route(
         route: Route,
         id: int,
         data_gen: RouteGenerator,
+        route_len_noiser: IntSampler,
         merch_item_noise_map: Dict[str, IntSampler],
         merch_len_noiser: IntSampler
 ) -> Route:
     # TODO: How do we add / remove routes?
     # TODO: Should we change from / to city with some probability?
+    new_route_len = len(route) + route_len_noiser.gen()
+
+    if new_route_len <= 0:
+        return NoisedRoute(id=id, route=[], parent_route_id=route.id)
+
+    extra_trips = []
+
+    if new_route_len <= len(route):
+        trips_to_noise = sample_items(
+            get_uni_dist_cat(route.route), new_route_len)
+    else:
+        trips_to_noise = list(route.route)
+        n_routes_to_gen = new_route_len - len(planned_routes)
+        prev_trip_city = trips_to_noise[-1].from_city
+        for i in range(n_routes_to_gen):
+            trip = data_gen.gen_trip(prev_trip_city)
+            extra_trips.append(trip)
+            prev_trip_city = extra_trips[i].from_city
+
     noised_trips = []
-    for trip in route:
+
+    for trip in trips_to_noise:
         noised_merch = []
 
         # Noise merch amount
         for merch_name, amount in trip.merch:
             merch_item_noise = merch_item_noise_map[merch_name].gen()
-            noised_amount = abs(amount + merch_item_noise)
-            noised_merch.append((merch_name, noised_amount))
+            noised_amount = amount + merch_item_noise
+            if noised_amount > 0:
+                noised_merch.append((merch_name, noised_amount))
 
         # Noise merch length
         merch_len_noise = merch_len_noiser.gen()
-        noised_merch_len = abs(merch_len_noise + len(trip.merch))
+        noised_merch_len = merch_len_noise + len(trip.merch)
+        if noised_merch_len <= 0:
+            break
 
         # If we have a shorter merch length, sample that amount of items from the merch
         if noised_merch_len < len(trip.merch):
@@ -263,7 +292,8 @@ def noise_route(
                 if n_new_merch > len(adjusted_dist):
                     n_new_merch = len(adjusted_dist)
 
-                new_merch = data_gen.gen_merch_items(n_new_merch, adjusted_dist)
+                new_merch = data_gen.gen_merch_items(
+                    n_new_merch, adjusted_dist)
                 noised_merch += list(new_merch)
             else:
                 noised_merch.append(trip.merch)
@@ -274,34 +304,42 @@ def noise_route(
             to_city=trip.to_city,
             merch=tuple(noised_merch)
         )
-        # print(trip)
-        # print(noised_trip)
         noised_trips.append(noised_trip)
 
+    noised_trips += extra_trips
+
+    if not noised_trips:
+        raise Exception(
+            "Noised trips such that no trips are left, please adjust parameters "
+            "so this cannot happen."
+        )
     return NoisedRoute(
         id=id,
         route=noised_trips,
         parent_route_id=route.id
     )
 
+
 # %%
 if __name__ == "__main__":
 
-    merch_opts = {"Apples", "Pears", "Bananas", "Kiwis", "Oranges", "Mandarins"}
+    start = time.time()
+    # Options for planned routes
+    N_PLANNED_ROUTES = 1_000
+
+    merch_items = {"Apples", "Pears", "Bananas",
+                   "Kiwis", "Oranges", "Mandarins"}
     cities = {"Amsterdam", "Utrecht", "Delft"}
 
-    merch_uni_dist = get_uni_dist_cat(merch_opts)
+    merch_uni_dist = get_uni_dist_cat(merch_items)
     city_uni_dist = get_uni_dist_cat(cities)
 
-    merch_len_sampler = UniformIntSampler(1, 3)
-
+    merch_len_sampler = UniformIntSampler(3, 5)
     merch_sampler_map = {
-        merch_name: UniformIntSampler(1, 100)
-        for merch_name in merch_opts
+        merch_name: UniformIntSampler(50, 100)
+        for merch_name in merch_items
     }
-
-    route_len_sampler = UniformIntSampler(1, 5)
-
+    route_len_sampler = UniformIntSampler(3, 5)
     generator = RouteGenerator(
         city_uni_dist,
         merch_uni_dist,
@@ -309,18 +347,35 @@ if __name__ == "__main__":
         merch_sampler_map,
         route_len_sampler
     )
+    planned_routes = [generator.gen_route(i) for i in range(N_PLANNED_ROUTES)]
 
-    N_ROUTES = 3
-    routes = [generator.gen_route(i) for i in range(N_ROUTES)]
+    # Options for planned routes
+    N_ACTUAL_ROUTES = 10_000
+
+    # How much each merch quantity will be noised
     merch_item_noise_map = {
-        merch_name: NormalIntSampler(-5, 5) for merch_name in merch_opts
+        merch_name: NormalIntSampler(-50, 50) for merch_name in merch_items
     }
-    merch_len_noiser = UniformIntSampler(0, 20)
-    route_to_noise = routes[0]
-    noised_route = noise_route(route_to_noise, 42, generator,
-                merch_item_noise_map, merch_len_noiser)
 
-    print("Original")
-    print(route_to_noise)
-    print("Noised")
-    print(noised_route)
+    # How big each trip merch length will be
+    merch_len_noiser = NormalIntSampler(-2, 2)
+    route_len_sampler = NormalIntSampler(-2, 2)
+
+    actual_routes = []
+    planned_routes_uni_dist = get_uni_dist_cat(planned_routes)
+    for i in range(N_ACTUAL_ROUTES):
+        picked_actual_route = sample_item(planned_routes_uni_dist)
+        actual_route = noise_route(
+            picked_actual_route,
+            i,
+            generator,
+            route_len_sampler,
+            merch_item_noise_map,
+            merch_len_noiser
+        )
+        actual_routes.append(actual_route)
+
+    print(planned_routes)
+    print(actual_routes)
+    end = time.time()
+    print(f"{end - start} seconds elapsed.")
