7a2caeb7b7cbea6492bc47fda9a12bf9fd44999a
Sjors Lockhorst <lockhorstsjors@gmail.com>
2023-06-25 13:56:25 +0200

    Added calculation of cost based on euclidian distance

--- src/find_similar.py
+++ src/find_similar.py
@@ -15,7 +15,7 @@ actual = actual.drop("route").cache()
 
 fit_start = time()
 brp = BucketedRandomProjectionLSH(
-    inputCol="route_vector", outputCol="hashes", numHashTables=2, bucketLength=10
+    inputCol="route_vector", outputCol="hashes", numHashTables=2, bucketLength=100
 )
 model = brp.fit(planned)
 print(f"Fitting LSH took {time() - fit_start}")
@@ -31,7 +31,7 @@ compare_start = time()
 result = model.approxSimilarityJoin(
     transformed_actual_df,
     transformed_planned_df,
-    threshold=10,
+    threshold=20,
     distCol="EuclideanDistance",
 ).cache()
 print(f"Did {result.count()} comparisons")
@@ -47,23 +47,59 @@ print(f"Found minima in {time() - min_start}s")
 join_start = time()
 
 # Join back to get the full row information from planned_df
-final_joined_result = nearest_neighbors.join(
+final_results = nearest_neighbors.join(
     F.broadcast(result),
     ["datasetA", "EuclideanDistance"]
 ).cache()
 print(f"Joined in {time() - join_start}s")
 
 # Evaluate the accuracy
-count = final_joined_result.filter(
-    final_joined_result.datasetA.original_route_uuid == final_joined_result.datasetB.uuid
+count = final_results.filter(
+    final_results.datasetA.original_route_uuid == final_results.datasetB.uuid
 ).count()
-average_dist = final_joined_result.agg(F.mean(F.col("EuclideanDistance"))).collect()[0][0]
-std_dist = final_joined_result.agg(F.stddev(F.col("EuclideanDistance"))).collect()[0][0]
-max_dist = final_joined_result.agg(F.max(F.col("EuclideanDistance"))).collect()[0][0]
-min_dist = final_joined_result.agg(F.min(F.col("EuclideanDistance"))).collect()[0][0]
+
 
 print(f"Number of rows where 'original_route_uuid' equals to 'uuid': {count}")
 print(f"Accuracy: {count / actual.count()}")
-print(f"EuclideanDistance: M={average_dist:.2f}; SD={std_dist:.2f}, min={min_dist}, max={max_dist}")
-print(f"{time() - global_start}s elapsed in total.")
 
+print("Calculating cost function based on norm. euclidean distance")
+
+
+euclid_cost_start = time()
+
+max_dist = final_results.agg(
+    F.max(F.col("EuclideanDistance"))).collect()[0][0]
+min_dist = final_results.agg(
+    F.min(F.col("EuclideanDistance"))).collect()[0][0]
+
+NORMALISATION_RECIPROCAL = 1 / (max_dist - min_dist)
+
+final_results = final_results\
+    .withColumn(
+        "NormEuclideanDistance",
+        (F.col("EuclideanDistance") - min_dist) * NORMALISATION_RECIPROCAL
+    )
+final_results = final_results\
+    .withColumn(
+        "Similarity",
+        1 - F.col("NormEuclideanDistance")
+    )
+
+final_results = final_results\
+    .withColumn(
+        "Payment",
+        F.col("Similarity") * 1000
+    ).cache()
+
+print(
+    f"Found cost based on euclidian similiarity in {time() - euclid_cost_start}s")
+
+print(final_results
+      .select(
+          ["EuclideanDistance", "NormEuclideanDistance", "Payment"]
+      )
+      .describe()
+      .show())
+
+
+print(f"{time() - global_start}s elapsed in total.")
